2e16c04af5075a0eecccd1a259d809d7f2a7b4f5
Avishai <avishome@gmail.com>
2020-10-20 21:51:09 +0300

    first commit

---
+++ precache-manifest.3a15cf08f83fd6caf85f2726ae9dd6bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5b727e3afdf8d3d5692e850f98f8d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93b108a48b77d3cdf188",
+    "url": "./static/css/main.22ffa98a.chunk.css"
+  },
+  {
+    "revision": "f86a6ad45383265d20eb",
+    "url": "./static/js/2.7f022a7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7f022a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b108a48b77d3cdf188",
+    "url": "./static/js/main.32ea395b.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "3a91a7e6d9da7a4aef048f609f873e7c",
+    "url": "./static/media/cloud-tools.3a91a7e6.svg"
+  },
+  {
+    "revision": "0f97fbfaeb50a2c2ff9a88e21b5482a1",
+    "url": "./static/media/shield.0f97fbfa.svg"
+  }
+]);
